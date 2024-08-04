2306ae9636a194b4329a11a5919624647a54b046
bailigo <bailey.everitt@oligosecurity.io>
2024-08-04 09:24:48 -0400

    Create cyclonedx-tasks.gradle.kts

---
+++ cyclonedx-tasks.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("org.cyclonedx.bom") version "1.8.2"
+}
+
+tasks.named<Jar>("bootJar") {
+    dependsOn(tasks.named("cyclonedxBom"))
+    from(file("$buildDir/reports")) {
+        include("application.cdx.json")
+        into("META-INF/sbom")
+    }
+}
+
+tasks.named<Jar>("jar") {
+    dependsOn(tasks.named("cyclonedxBom"))
+    from(file("$buildDir/reports")) {
+        include("application.cdx.json")
+        into("META-INF/sbom")
+    }
+}
+
+tasks.named<org.cyclonedx.gradle.CycloneDxTask>("cyclonedxBom") {
+    outputName.set("application.cdx")
+    outputFormat.set("json")
+    schemaVersion.set("1.5")
+    destination.set(file("build/reports"))
+    includeConfigs.set(listOf("runtimeClasspath", "compileClasspath"))
+    skipConfigs.set(emptyList())
+    skipProjects.set(emptyList())
+}
